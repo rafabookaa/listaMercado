907cc81e9d8e121e01c7640c13e5204c6bf22dc4
Rafael Ribeiro <59180005+rafabookaa@users.noreply.github.com>
2023-07-03 14:12:31 -0300

    Interação concluída

--- main.js
+++ main.js
@@ -1,37 +1,54 @@
+let categoria;
 let frutas = [];
 let laticinios = [];
 let congelados = [];
 let doces = [];
-let confirmacao;
-let categoria;
-let entradaItem;
 
-let input = document.querySelector('input')
-//input.value();
+janelaInicial = confirm(`Bem vindo ao site que registra ibtem de supermercado`)
 
-function perguntaAdicao () {
-    confirmacao = confirm('Deseja adicionar algum item na lista?')
+if(janelaInicial == true) {
+    pergunta();
+  }
 
-    if (confirmacao == true) {
-        categoria = prompt('Entre com a categoria de produtos que deseja adicionar a lista: \n 1 - Frutas \n 2 - Laticinios \n 3 - Conlelados \n 4 - Doces')
-    }else {
-    alert(`Sua lista de compras é: ${frutas}`);
-        }
-}
+function pergunta () {
+let perguntaAdicionar = confirm('Deseja adicionar um item á lista?')
 
-function adiciona () {
-
-    if(categoria == 1) {
-        frutas = input.value;
-        frutas.push;
-        perguntaAdicao();
-    }
+if(perguntaAdicionar) {
+    categoria = prompt("Digite a opção para adicionar o item conforme sua categoria: \n 1 - Fruta \n 2 - Laticinios \n 3 - Congelados \n 4 - Doces")
     
+} if(perguntaAdicionar == false){
+
+let impressao = confirm('Deseja imprimir a lista de itens adicionados?')
+
+  if(impressao == true){
+    alert(`Sua lista de compras é: \n Frutas: \n ${frutas} \n \n Laticinios: \n ${laticinios} \n \n Congelados: \n ${congelados} \n \n Doces: \n ${doces}`);
+  } 
 }
 
-let button = document.querySelector('button')
-button.onclick = adiciona;
+}
+let input = document.querySelector('.input_botao')
+input.focus();
 
-perguntaAdicao();
+function adicionaItem () {    
+        
+      if(categoria == 1){
+        frutas.push(input.value)        
+      }      
+      if(categoria == 2){
+        laticinios.push(input.value)        
+      }      
+      if(categoria == 3){
+        congelados.push(input.value)
+      }      
+      if(categoria == 4){
+        doces.push(input.value)       
+      }    
+        input.value = " ";
+        input.focus();  
+        pergunta();    
+    }
+        
+let button = document.querySelector('button');
+button.onclick = adicionaItem;
 
 
